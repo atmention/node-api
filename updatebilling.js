8d97f76f30a1a65544686df517d094c501325eec
atmention <tpc303@gmail.com>
2022-07-12 19:04:53 -0600

    fixes

--- updatebilling.js
+++ updatebilling.js
@@ -1,7 +1,7 @@
 const recurly = require('recurly')
 // You should store your API key somewhere safe
 // and not in plain text if possible
-const myApiKey = '1c2dc01087ff4093a01dd51116db62c1' //add api key here
+const myApiKey = '' //add api key here
 const client = new recurly.Client(myApiKey)
 
 
